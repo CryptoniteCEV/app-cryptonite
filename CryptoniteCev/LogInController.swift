b57449b2313fb36abca9b388a6beb38e58e42684
JoseAlejandroGarciaCifuentes <jose_alejandro_garcia_apps1ma1920@cev.com>
2021-01-20 20:15:05 +0100

    Few corrections in variable names

---
+++ CryptoniteCev/LogInController.swift
@@ -0,0 +1,18 @@
+
+
+import UIKit
+
+class LogInController: UIViewController {
+
+    @IBOutlet weak var logInButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        logInButton.layer.cornerRadius = 5
+    }
+
+
+    @IBAction func validation(_ sender: Any) {
+        
+    }
+}
+
