a53a86c4ef24167c2c6c8cb288d6687933832773
JoseAlejandroGarciaCifuentes <jose_alejandro_garcia_apps1ma1920@cev.com>
2021-01-21 19:55:08 +0100

    Cocoapods init and Alamofire installed

---
+++ Pods/Target Support Files/Pods-CryptoniteCev-CryptoniteCevUITests/Pods-CryptoniteCev-CryptoniteCevUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CryptoniteCev_CryptoniteCevUITests : NSObject
+@end
+@implementation PodsDummy_Pods_CryptoniteCev_CryptoniteCevUITests
+@end
