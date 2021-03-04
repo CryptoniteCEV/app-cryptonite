5b7383ac187843eb8971ea983ced7974deac5c01
JoseAlejandroGarciaCifuentes <jose_alejandro_garcia_apps1ma1920@cev.com>
2021-03-04 02:07:53 +0100

    tab bar reorganized, first screen is now the middle one

---
+++ CryptoniteCev/Controllers/TabBarController.swift
@@ -0,0 +1,18 @@
+//
+//  TabBarController.swift
+//  CryptoniteCev
+//
+//  Created by Alejandro García on 4/3/21.
+//  Copyright © 2021 user177257. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TabBarController:UITabBarController{
+    
+    override func viewDidLoad() {
+        self.selectedIndex = 1
+    }
+    
+}
