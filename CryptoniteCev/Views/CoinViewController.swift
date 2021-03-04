073d4699a9b131d52fbfb6adb27861d78ef57657
JesusTalayeroP <jesus_talayero_apps1ma@cev.com>
2021-03-04 20:50:34 +0000

    CoinViewController added, mostly finished need to change some icons

---
+++ CryptoniteCev/Views/CoinViewController.swift
@@ -0,0 +1,45 @@
+
+
+import UIKit
+import Charts
+
+class CoinViewController: UIViewController, ChartViewDelegate {
+    
+    @IBOutlet weak var containerView: UIView!
+    
+    @IBOutlet weak var tradeButton: UIButton!
+    
+    @IBOutlet weak var coinIconIV: UIImageView!
+    
+    @IBOutlet weak var currencyNameL: UILabel!
+    
+    @IBOutlet weak var coinValueL: UILabel!
+    
+    @IBOutlet weak var coinPercentageL: UILabel!
+    
+    var graph = PieChart()
+    var lineChart = LineChartView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        lineChart.delegate = self
+        
+        tradeButton.layer.cornerRadius = tradeButton.frame.height / 8
+
+    }
+    
+    @IBAction func TradeViewButton(_ sender: Any) {
+        performSegue(withIdentifier: "TradeViewID", sender: sender)
+        
+    }
+    
+    
+    override func viewDidLayoutSubviews() {
+     super.viewDidLayoutSubviews()
+    
+        graph.impirmirGrafica(lineChart: lineChart, screen: containerView)
+            
+    }
+
+}
