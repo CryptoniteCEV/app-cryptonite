1310ce7d07bea65c0121ac1b8fb6626074709636
SERGIO_SUAREZ APPS1MA1920 <sergio_suarez_apps1ma1920@cev.com>
2021-03-05 14:47:00 +0100

    PieChart Done Boys Jesus Tiene Envidiah

--- CryptoniteCev/Helpers/PieChart.swift
+++ CryptoniteCev/Helpers/PieChart.swift
@@ -11,8 +11,11 @@ class PieChart: UIViewController, ChartViewDelegate{
             height: screen.frame.height
                    
                )
+        pieChart.rotationAngle = 0
+        pieChart.rotationEnabled = false
         
-        print("está haciendo la funcion")
+        pieChart.drawHoleEnabled = false
+        //5print("está haciendo la funcion")
            
         screen.addSubview(pieChart)
            
@@ -21,23 +24,45 @@ class PieChart: UIViewController, ChartViewDelegate{
         pieChart.trailingAnchor.constraint(equalTo: screen.trailingAnchor, constant: -screen.frame.width).isActive = true
         pieChart.heightAnchor.constraint(equalToConstant: screen.frame.height).isActive = true
 
-        var entries = [ChartDataEntry]()
         
-        for x in 0...10{
-           
-            entries.append(ChartDataEntry(
-               
-               x: Double(x),
-               y: Double(x)
-               
-           ))
-           
-        }
+        let etheriumEntry: Double = 20
+        let bitcoinEntry: Double = 20
+        let dogeEntry: Double = 20
+        let liteEntry: Double = 20
+        let tetherEntry: Double = 20
+        
+        var entries : [PieChartDataEntry] = Array()
+        //Entrada de etherium
+        entries.append(PieChartDataEntry(value: etheriumEntry, label: "ETH"))
+        //entrada de Bitcoin
+        entries.append(PieChartDataEntry(value: bitcoinEntry, label: "BTC"))
+        //entrada de Dogecoin
+        entries.append(PieChartDataEntry(value: dogeEntry, label: "DOGE"))
+        //entrada de litecoint
+        entries.append(PieChartDataEntry(value: liteEntry, label: "LITE"))
+        //entrada de tether
+        entries.append(PieChartDataEntry(value: tetherEntry, label: "USDT"))
             
         let set =  PieChartDataSet(entries: entries)
-
-        set.colors = ChartColorTemplates.material()
-        set.valueTextColor = UIColor.white
+        
+        
+        
+        let etheriumColor = NSUIColor(red: 98/255.0, green: 126/255.0, blue: 224/255.0, alpha: 0.95)
+        
+        let bitcoinColor = NSUIColor(red: 242/255.0, green: 127/255.0, blue: 27/255.0, alpha: 0.95)
+        let dogeColor = NSUIColor(red: 244/255.0, green: 245/255.0, blue: 121/255.0, alpha: 0.96)
+        let liteColor = NSUIColor(red: 192/255.0, green: 192/255.0, blue: 192/255.0, alpha: 0.75)
+        let tetherColor = NSUIColor(red: 80/255.0, green: 175/255.0, blue: 149/255.0, alpha: 0.69)
+        
+        
+        let valueColor = NSUIColor(red: 0/255.0, green: 0/255.0, blue: 0/255.0, alpha: 0.95)
+        let labelEntryColor = NSUIColor(red: 0/255.0, green: 0/255.0, blue: 0/255.0, alpha: 0.95)
+        
+        set.colors = [etheriumColor, bitcoinColor,dogeColor,liteColor,tetherColor]
+        set.valueColors =  [valueColor]
+        set.entryLabelColor = labelEntryColor
+        
+        
         
       
         let data = PieChartData(dataSet: set)
--- Pods/Charts/Source/Charts/Utils/ChartColorTemplates.swift
+++ Pods/Charts/Source/Charts/Utils/ChartColorTemplates.swift
@@ -14,11 +14,25 @@ import CoreGraphics
 
 open class ChartColorTemplates: NSObject
 {
+    
+    @objc open class func currencies () -> [NSUIColor]
+    {
+        return [
+            NSUIColor(red: 242/255.0, green: 127/255.0, blue: 27/255.0, alpha: 0.95),
+            NSUIColor(red: 148/255.0, green: 212/255.0, blue: 212/255.0, alpha: 1.0),
+            NSUIColor(red: 136/255.0, green: 180/255.0, blue: 187/255.0, alpha: 1.0),
+            NSUIColor(red: 118/255.0, green: 174/255.0, blue: 175/255.0, alpha: 1.0),
+            NSUIColor(red: 42/255.0, green: 109/255.0, blue: 130/255.0, alpha: 1.0)
+        ]
+    }
+
+
+    
     @objc open class func liberty () -> [NSUIColor]
     {
         return [
-            NSUIColor(red: 207/255.0, green: 248/255.0, blue: 246/255.0, alpha: 1.0),
-            NSUIColor(red: 148/255.0, green: 212/255.0, blue: 212/255.0, alpha: 1.0),
+             NSUIColor(red: 242/255.0, green: 127/255.0, blue: 27/255.0, alpha: 0.95),
+             NSUIColor(red: 0/255.0, green: 0/255.0, blue: 0/255.0, alpha: 0.92),
             NSUIColor(red: 136/255.0, green: 180/255.0, blue: 187/255.0, alpha: 1.0),
             NSUIColor(red: 118/255.0, green: 174/255.0, blue: 175/255.0, alpha: 1.0),
             NSUIColor(red: 42/255.0, green: 109/255.0, blue: 130/255.0, alpha: 1.0)
