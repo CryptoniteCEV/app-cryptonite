dff21234385c5386b23c800bb009d5ef4d2ec4fd
Sharkteeth89 <Sharkteeth89spain@gmail.com>
2021-03-05 14:55:15 +0100

    Added wallet button to gamification

--- CryptoniteCev/Views/GamificationController.swift
+++ CryptoniteCev/Views/GamificationController.swift
@@ -36,21 +36,25 @@ class GamificationController: UIViewController {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.title = "Rewards"
         clearMission1.layer.cornerRadius = clearMission1.frame.width/2
         clearMission2.layer.cornerRadius = clearMission1.frame.width/2
         clearMission3.layer.cornerRadius = clearMission1.frame.width/2
         circleProgressBar.setProgress(0.3, animated: true)
         self.viewConfeti = SwiftConfettiView(frame: self.view.bounds)
-        
+        /*
+            self.viewConfeti?.type = .image(#imageLiteral(resourceName: "btc"))
+            self.viewConfeti?.colors = [#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)]
+            self.viewConfeti?.intensity=0.69
+        */
         self.viewConfeti?.tag = 200
         missionsCard.layer.cornerRadius = 10
         walletCard.layer.cornerRadius = 10
         profileImage.layer.cornerRadius = profileImage.frame.width / 2
         profileImage.layer.borderWidth = 4
         profileImage.layer.borderColor = #colorLiteral(red: 0.262745098, green: 0.8509803922, blue: 0.7411764706, alpha: 1)
-    
-        
-        
+    }
+    override func viewDidAppear(_ animated: Bool) {
         
     }
     
