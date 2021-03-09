1f00e752efc23ab5d6a98b4237086f3fd6b68d6f
Sharkteeth89 <Sharkteeth89spain@gmail.com>
2021-03-09 14:52:09 +0100

    Refactored gamification buttons and added missions

---
+++ CryptoniteCev/Constants/Missions.swift
@@ -0,0 +1,20 @@
+//
+//  Missions.swift
+//  CryptoniteCev
+//
+//  Created by alumnos on 09/03/2021.
+//  Copyright © 2021 user177257. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Missions{
+    
+    var missions = [ Mission(id: 0, image: #imageLiteral(resourceName: "023-coin stacks"), title: "Buy DOGE"),
+                     Mission(id: 1, image: #imageLiteral(resourceName: "034-trading"), title: "Check Bitcoin"),
+                     Mission(id: 2, image: #imageLiteral(resourceName: "049-currency exchange"), title: "Buy any cryptocurrency"),
+                     Mission(id: 3, image: #imageLiteral(resourceName: "045-digital investment"), title: "Follow someone"),
+                     Mission(id: 4, image: #imageLiteral(resourceName: "021-Litecoin"), title: "Sell LTC"),
+                     Mission(id: 5, image: #imageLiteral(resourceName: "008-cryptovault"), title: "Sell everything to USDT")]
+}
--- CryptoniteCev/Views/GamificationController.swift
+++ CryptoniteCev/Views/GamificationController.swift
@@ -18,29 +18,43 @@ class GamificationController: UIViewController {
     
     @IBOutlet var walletCard: UIView!
     var viewConfeti: SwiftConfettiView?
-
-    @IBOutlet var clearMission1: UIButton!
     
-    @IBOutlet var clearMission2: UIButton!
+    @IBOutlet weak var mission1Button: UIButton!
     
-    @IBOutlet var clearMission3: UIButton!
+    @IBOutlet weak var mission2Button: UIButton!
+    
+    @IBOutlet weak var mission3Button: UIButton!
+    
+    
+    
+    
     
     @IBOutlet var profileImage: UIImageView!
     
     @IBOutlet weak var cashLabel: UILabel!
     
-    @IBAction func claimRewards(_ sender: UIButton) {
-        
-        self.startConfetti(view: self.viewConfeti!)
-        self.stopConfetti(view: self.viewConfeti!)
-        
+    var experience : Int = 0
+    
+    var experiencePerMission = 200
+    
+    @IBAction func mission1Cleared(_ sender: UIButton) {
+        claimRewards()
     }
+    
+    @IBAction func mission2Cleared(_ sender: UIButton) {
+        claimRewards()
+    }
+    
+    @IBAction func mission3Cleared(_ sender: UIButton) {
+        claimRewards()
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Rewards"
-        clearMission1.layer.cornerRadius = clearMission1.frame.width/2
-        clearMission2.layer.cornerRadius = clearMission1.frame.width/2
-        clearMission3.layer.cornerRadius = clearMission1.frame.width/2
+        mission1Button.layer.cornerRadius = mission1Button.frame.width/2
+        mission2Button.layer.cornerRadius = mission2Button.frame.width/2
+        mission3Button.layer.cornerRadius = mission3Button.frame.width/2
         circleProgressBar.setProgress(0.3, animated: true)
         self.viewConfeti = SwiftConfettiView(frame: self.view.bounds)
         /*
@@ -54,6 +68,7 @@ class GamificationController: UIViewController {
         profileImage.layer.cornerRadius = profileImage.frame.width / 2
         profileImage.layer.borderWidth = 4
         profileImage.layer.borderColor = #colorLiteral(red: 0.262745098, green: 0.8509803922, blue: 0.7411764706, alpha: 1)
+        
         let level = GetLevel(exp: 200)
         let nextlevel = NextLevelExp(level: level)
         let expLeft = nextlevel - 200
@@ -87,6 +102,11 @@ class GamificationController: UIViewController {
             let confeti = self.view.viewWithTag(200)
             confeti!.removeFromSuperview()
         }
+    }
+    
+    func claimRewards() {
+        self.startConfetti(view: self.viewConfeti!)
+        self.stopConfetti(view: self.viewConfeti!)
     }
     
     func GetLevel(exp: Int) -> Int {
